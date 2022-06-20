d8c18dc1eced444b367904295c40171646775cc2
Mohamed Ramadan <mohamedramadan6420@gmail.com>
2022-06-20 18:22:19 +0200

    AdminLayout

--- lib/modules/pay_fare/wallet/request_credit.dart
+++ lib/modules/pay_fare/wallet/request_credit.dart
@@ -62,7 +62,7 @@ class RequestCreditScreen extends StatelessWidget {
                       navigateTo(context,ListViewPage(amountToPay: double.parse(numberController.text)));
                     },
                     text: 'Next')
-//
+////
               ],
             ),
           ),
